3e90b9c0561e2677d45e9dc8f9c57ac8805a6536
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2022-02-24 15:25:23 -0400

    feat: Add htmlfile listener

---
+++ src/htmlFile.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+
+const requestListener = function (req, res) {
+  fs.readFile(__dirname + '/index.html')
+    .then((contents) => {
+      res.setHeader('Content-Type', 'text/html');
+      res.writeHead(200);
+      res.end(contents);
+    })
+    .catch((err) => {
+      res.writeHead(500);
+      res.end(err);
+      return;
+    });
+};
+
+module.exports = requestListener;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const http = require('http');
 const host = 'localhost';
 const port = '8000';
 
+const htmlFileListener = require('./htmlFile');
+
 const requireListener = function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
   res.writeHead(200);
-  res.end('My first server');
+  res.end(`{"message": "This is a JSON response"}`);
 };
 
-const server = http.createServer(requireListener);
+const server = http.createServer(htmlFileListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
