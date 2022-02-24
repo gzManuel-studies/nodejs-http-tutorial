78e2b09d522599b31bc756dc376996190b1f703e
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2022-02-24 15:01:32 -0400

    chore: Add eslint with prettier

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ const http = require('http');
 const host = 'localhost';
 const port = '8000';
 
-const requireListener = function(req, res) {
-    res.writeHead(200);
-    res.end('My first server');
-    
-}
+const requireListener = function (req, res) {
+  res.writeHead(200);
+  res.end('My first server');
+};
 
 const server = http.createServer(requireListener);
